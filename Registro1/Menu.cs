d53eb61452875776c85d1a49bf868f338d576737
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-09-04 10:28:13 -0400

    Jerarquia de N capas creadas

--- Registro1/Menu.cs
+++ Registro1/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Registro1
 {
-    public partial class Form1 : Form
+    public partial class EstudianteForm : Form
     {
-        public Form1()
+        public EstudianteForm()
         {
             InitializeComponent();
         }
