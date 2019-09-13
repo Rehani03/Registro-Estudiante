073172405b3c34762b78f5124a8a2137285e5741
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2019-09-13 10:26:58 -0400

    Debug Test completado.

---
+++ Registro1Tests/BLL/InscripcionBLLTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Registro1.BLL;
+using Registro1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro1.BLL.Tests
+{
+    [TestClass()]
+    public class InscripcionBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            bool paso;
+            Inscripcion i = new Inscripcion();
+            i.InscripcionID = 0;
+            i.Fecha = DateTime.Now;
+            i.EstudianteID = 4;
+            i.Comentario = "Test Debug";
+            i.Deposito = 1000;
+            i.Monto = 500;
+            paso = InscripcionBLL.Guardar(i);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            bool paso;
+            Inscripcion i = new Inscripcion();
+            i.InscripcionID = 1;
+            i.Fecha = DateTime.Now;
+            i.EstudianteID = 1;
+            i.Comentario = "Test Debug";
+            i.Deposito = 1000;
+            i.Monto = 500;
+            paso = InscripcionBLL.Modificar(i);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            bool paso;
+            paso = InscripcionBLL.Eliminar(2, 2);
+            Assert.AreEqual(paso, true);
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Inscripcion i; 
+            i = InscripcionBLL.Buscar(3);
+            Assert.AreEqual(i, i);
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            var listado = new List<Inscripcion>();
+            listado = InscripcionBLL.GetList(p => true);
+            Assert.AreEqual(listado, listado);
+        }
+    }
+}
